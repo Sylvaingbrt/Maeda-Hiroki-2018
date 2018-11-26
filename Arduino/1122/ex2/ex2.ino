1be4ac8c9c3a63809e450e5f1116338f9224d7e2
Hiroki_Maeda <q238059h@mail.kyutech.jp>
2018-11-26 20:28:28 +0900

    ex2

---
+++ Arduino/1122/ex2/ex2.ino
@@ -0,0 +1,27 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(6,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  int i;
+
+  for(i=0;i<10;i++){
+   analogWrite(6,255*i*0.1); 
+   delay(500);
+    }
+      for(i=10;i>0;i--){
+   analogWrite(6,255*i*0.1); 
+   delay(500);
+    }
+
+/*
+for(i=0;i<2*3.14;i+=0.01){
+  analogWrite(6,255*abs(sin(i*3.14)));
+  delay(10);
+  }
+*/
+}
